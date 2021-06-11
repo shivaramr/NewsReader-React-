078eb35520c1d937262c884ea58155cd56b119d5
shivaramr <85446156+shivaramr@users.noreply.github.com>
2021-06-11 16:04:42 +0530

    update search feature

--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -6,14 +6,46 @@ import SearchIcon from "@material-ui/icons/Search";
 import { TextField } from "@material-ui/core";
 import { newsContext } from "../../Helper/Context";
 import "./topStyle.css";
+import { url } from "../../Helper/API";
 
 function TopNav() {
-  const { setQueryObj } = useContext(newsContext);
+  const { dateRange, queryObj, setQueryObj, setNews, setNext, setErr } = useContext(newsContext);
 
   const searchChange = (e) => {
     let obj = { q: e.target.value };
     setQueryObj(obj);
-    console.log('shiv', obj);
+  };
+
+  const searchResult = () => {
+    let resultURL = url;
+
+    if (dateRange.start_date && dateRange.start_date.length) {
+      resultURL =
+        resultURL +
+        `&start_date=${dateRange.start_date}&end_date=${dateRange.end_date}`;
+    }
+    if (queryObj.q) resultURL = resultURL + `&q=${queryObj.q}`;
+    if (queryObj.category_id)
+      resultURL = resultURL + `&category_id=${queryObj.category_id}`;
+    if (queryObj.sentiment)
+      resultURL = resultURL + `&sentiment=${queryObj.sentiment}`;
+    if (queryObj.source_id)
+      resultURL = resultURL + `&source_id=${queryObj.source_id}`;
+
+      fetch(resultURL)
+      .then((response) => {
+        if (response.status !== 200)
+          throw new Error("Server responds with error");
+        return response.json();
+      })
+      .then(
+        (data) => {
+          setNews(data.result.data);
+          setNext(data.result.nextUrl);
+        },
+        (err) => setErr(err)
+      );
+
   };
 
   return (
@@ -28,7 +60,7 @@ function TopNav() {
           className="searchField"
           variant="outlined"
           InputProps={{
-            startAdornment: <SearchIcon />,
+            startAdornment: <SearchIcon onClick={searchResult} className="search" />,
           }}
           onChange={searchChange}
         />
